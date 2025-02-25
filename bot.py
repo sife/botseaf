5e95d330490f91c56dd465996345f60b56543155
sife <sa_9_8@yahoo.com>
2025-02-25 03:28:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,5 +84,5 @@ async def main():
     await application.run_polling()
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
